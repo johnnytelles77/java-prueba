027dd8088482f1991c57709625882f9223245f9f
johnnytelles77 <137516577+johnnytelles77@users.noreply.github.com>
2024-10-04 19:02:22 -0400

    clase01

---
+++ clase01/src/main/java/com/coderhouse/clase01.java
@@ -0,0 +1,39 @@
+package com.coderhouse;
+
+public class clase01 {
+
+	public static void main(String[] args) {
+
+		/*int variableA = 1000;
+		
+		byte variableB = 12;
+		
+		String numeroA = "Hola";
+		
+		char letra = '@';
+		String letraA = "b";
+		
+		String letraB = "hjhf";
+		
+	//System.out.printIn("El resultado es: " + (variableA + variableB))
+		
+		System.out.println(variableA);*/
+		
+		/*float numero1 = 12f;
+		float numero2 = 12f;
+		float resultado;
+		
+		resultado = numero1 + numero2;
+		
+		System.out.println("El resultado es:" + resultado);*/
+		
+		int contador = 1;
+		
+		System.out.println(contador);
+		contador++;
+		System.out.println(contador);
+		
+
+	}
+
+}
